6f512074ed4e2be7eb357cb48e4b24f5693bc405
Anthony Harrison <anthony.harrison@xamtastic.com>
2017-11-19 16:54:26 +0000

    MvvmCross notes followed, produces a Window

---
+++ Azure.Screenshots/Azure.ScreenShots.Mac/Views/FirstViewController.designer.cs
@@ -0,0 +1,9 @@
+﻿namespace Azure.ScreenShots.Mac.Views
+{
+
+    // Should subclass AppKit.NSViewController
+    [Foundation.Register("FirstViewController")]
+    public partial class FirstViewController
+    {
+    }
+}
